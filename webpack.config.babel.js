c08c181e8121dc989f5ca08dba490be4de90f223
monospaced <scott@monospaced.com>
2017-03-10 13:36:16 +0000

    Production mode for redux

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -58,6 +58,11 @@ const config = {
 if (production) {
   config.plugins = [
     ...config.plugins,
+    new webpack.DefinePlugin({
+      'process.env': {
+         NODE_ENV: `'production'`,
+       },
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
